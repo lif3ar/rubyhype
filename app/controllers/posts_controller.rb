c26856d8d09d093bc1ff9234c50bb9c1a39c9fa7
Petr Sergeevich <ossipov.pietro@gmail.com>
2016-05-12 13:49:00 +0300

    Add comments to posts and Users association

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -22,6 +22,7 @@ class PostsController < ApplicationController
   end
 
   def show
+    @comments = Comment.where(post_id: @post)
   end
 
   def edit
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ActiveRecord::Base
   belongs_to :user
+  has_many :comments, dependent: :destroy
 
   has_attached_file :image, styles: { small: "350x250>", medium: "700x500#" },
                     default_url: "/images/:style/missing.png"
