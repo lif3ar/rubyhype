5db79c41fb336966013200a24a0cd15903e4604a
Petr Sergeevich <ossipov.pietro@gmail.com>
2016-05-12 12:51:07 +0300

    Add paperclip and views

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,8 @@
 class Post < ActiveRecord::Base
   belongs_to :user
+
+  has_attached_file :image, styles: { small: "350x250>", medium: "700x500#" },
+                    default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+
 end
