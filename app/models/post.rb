efb2693eb5c02200a8c771996caf8dfc3dfa1e57
Petr Sergeevich <ossipov.pietro@gmail.com>
2016-05-13 17:36:14 +0300

    Fixed buttons, alerts ant notices styling and changed content

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,4 +7,7 @@ class Post < ActiveRecord::Base
                     default_url: "/images/:style/missing.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 
+  validates :image, presence: true
+  validates :description, presence: true
+  validates :title, presence: true
 end
